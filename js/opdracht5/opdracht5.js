4f95fe0733e6ca9cbf104db8b03994b6eff9f3c9
Krouni <92445948+Krouni@users.noreply.github.com>
2022-03-01 21:27:16 +0100

    Delete js/opdracht5 directory

--- js/opdracht5/opdracht5.js
+++
@@ -1,9 +0,0 @@
-const setup = () => {
-    let button = document.getElementById("button");
-    button.addEventListener("click", setup2);
-}
-const setup2 = () =>{
-    let pElement=document.getElementById("txtOutput");
-    pElement.innerHTML="welkom";
-}
-window.addEventListener("load", setup);

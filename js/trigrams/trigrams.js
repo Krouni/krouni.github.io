dd68afff823f1a677c643aeb6aa9b8b594d9e18d
Krouni <92445948+Krouni@users.noreply.github.com>
2022-03-30 11:40:11 +0200

    Delete js/trigrams directory

--- js/trigrams/trigrams.js
+++
@@ -1,9 +0,0 @@
-const setup = () => {
-    let word = 'onoorbaar';
-
-    for(let i = 0; i+2 < word.length; i++)
-    {
-        console.log(word[i] + word[i+1] + word[i+2]);
-    }
-}
-window.addEventListener("load", setup);

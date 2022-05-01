4aa1144ff10c7b8c9c15bbb73557f22b3000e68b
Krouni <92445948+Krouni@users.noreply.github.com>
2022-05-01 08:57:53 +0200

    Delete js/JSON2 directory

--- js/JSON2/json.js
+++
@@ -1,6 +0,0 @@
-const setup = () => {
-    let tekst = "{\"voornaam\":\"Jan\",\"familienaam\":\"Janssens\",\"geboorteDatum\":null,\"adres\":{\"straat\":\"Kerkstraat 13\",\"postcode\":\"8500\",\"gemeente\":\"Kortrijk\"},\"isIngeschreven\":true,\"namenVanExen\":[\"Sofie\",\"Berta\",\"Philip\",\"Albertoooo\"],\"aantalAutos\":2}";
-    let student = JSON.parse(tekst);
-    console.log(student.voornaam);
-}
-window.addEventListener("load",setup);
